d4c209faba1c2a423e990452aa68ee31fbe03120
gnajabi <gnajabi123@gmail.com>
2022-11-14 15:28:47 +0300

    images added

--- happy and sad/myScript.js
+++ happy and sad/myScript.js
@@ -24,6 +24,7 @@ function goodbyeStranger(){
 }
 
 function helloAgain(){
-    document.getElementById('title').innerHTML="Ahoy!"
+    document.getElementById('title').innerHTML="Boo!"
+    document.getElementById('image').src="enemy-ghost-icon.png"
     hereIAm()
 }
